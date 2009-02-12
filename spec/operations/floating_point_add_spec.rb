471603d44a2c4b48674c7f41bc195d627074afbb
buntine <andrew.buntine@ansearch.com>
2009-02-12 13:57:06 +1100

    Cleaned up floating point operation specs slightly and added note about shitty floating points in README

--- spec/operations/floating_point_add_spec.rb
+++ spec/operations/floating_point_add_spec.rb
@@ -8,16 +8,14 @@ describe Bolverk::Operations::FloatingPointAdd do
 
   describe "when executing operation" do
 
-    before do
+    it "should add 1 1/8 and 1/2 and store the result in register 4" do
+      # Setup program to run.
       @machine = Bolverk::Emulator.new
       @machine.register_write("2", "59") # 1 and 1/8 (1.125) in hex.
       @machine.register_write("3", "54") # 1/2 (0.5) in hex.
-
       @machine.load_program_into_memory("B1", [ '6234', 'C000' ])
       @machine.start_program "B1"
-    end
-
-    it "should add registers 2 and 3, storing the value in register 4" do
+ 
       @machine.registers[4].should eql("00000000")
       @machine.perform_machine_cycle
 
@@ -25,28 +23,22 @@ describe Bolverk::Operations::FloatingPointAdd do
       @machine.registers[4].should eql("01011101")
     end
 
-    it "should raise error in the case of an overflow"
-
-  end
-
-  describe "when executing operation again" do
-
-    before do
+    it "should add 1/16 and 1/32 and store the result in register 4" do
+      # Setup program to run.
       @machine = Bolverk::Emulator.new
-      @machine.register_write("2", "41") # 1/16 (1.125) in hex.
-      @machine.register_write("3", "31") # 1/32 (0.5) in hex.
-
+      @machine.register_write("2", "41") # 1/16 (1.0625) in hex.
+      @machine.register_write("3", "31") # 1/32 (0.03125) in hex.
       @machine.load_program_into_memory("B1", [ '6234', 'C000' ])
       @machine.start_program "B1"
-    end
-
-    it "should add registers 2 and 3, storing the value in register 4" do
+ 
       @machine.registers[4].should eql("00000000")
       @machine.perform_machine_cycle
 
       # NOTE: 41 (1.0625) + 31 (0.03125) = 0101110 (1.09375 or 3/31)
       @machine.registers[4].should eql("00110011")
     end
+
+    it "should raise error in the case of an overflow"
 
   end
 
